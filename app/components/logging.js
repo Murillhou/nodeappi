1a65affbf7cf2f0f8541109a9e78c5d678fc99ce
Murillhou <pmurillo@makenai.es>
2017-11-07 03:27:52 +0100

    Logging fixed for Heroku.

--- app/components/logging.js
+++ app/components/logging.js
@@ -3,35 +3,37 @@ const fs = require('fs'),
   PrettyStream = require('bunyan-prettystream');
 
 module.exports = name => {
-  const prettyFile = new PrettyStream();
-  prettyFile.pipe(fs.createWriteStream(`/var/log/pretty_${name}_log.json`));
-  const prettyStdOut = new PrettyStream();
-  prettyStdOut.pipe(process.stdout);
+  // const prettyFile = new PrettyStream();
+  // prettyFile.pipe(fs.createWriteStream(`/var/log/pretty_${name}_log.json`));
+  // const prettyStdOut = new PrettyStream();
+  // prettyStdOut.pipe(process.stdout);
   const logger = bunyan.createLogger({
     name,
     type: 'raw',
-    streams: [{
-        level: 'info',
-        stream: prettyFile,
-      },
+    streams: [
+      // {
+      //   level: 'info',
+      //   stream: prettyFile,
+      // },
+      // {
+      //   level: 'info',
+      //   stream: fs.createWriteStream(`/var/log/${name}_log.json`)
+      // },
       {
         level: 'info',
-        stream: fs.createWriteStream(`/var/log/${name}_log.json`)
-      },
-      {
-        level: 'info',
-        stream: prettyStdOut
+        stream: process.stdout
+          //stream: prettyStdOut
       }
     ]
   });
   return {
     error: (err, ...rest) => {
       logger.error(err, rest.length ? rest.join() : '');
-      console.log(' ' + name + '-' + Date.now() + ': ', err, rest.length ? rest.join() : '');
+      //console.log(' ' + name + '-' + Date.now() + ': ', err, rest.length ? rest.join() : '');
     },
     log: (msg, ...rest) => {
       logger.info(msg, rest.length ? rest.join() : '');
-      console.log(' ' + name + '-' + Date.now() + ': ', msg, rest.length ? rest.join() : '');
+      //console.log(' ' + name + '-' + Date.now() + ': ', msg, rest.length ? rest.join() : '');
     }
   };
 };
